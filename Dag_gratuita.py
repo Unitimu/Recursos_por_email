e282cf52e19e991062de6228de7d13af71e90807
george.mendes <george.mendes@mjv.com.br>
2022-06-13 22:50:28 -0300

    feat - Agora é possível verificar novidades em sites externos!

--- Dag_gratuita.py
+++ Dag_gratuita.py
@@ -8,14 +8,19 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import pymongo
 from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+import requests
+import re
+
 
 # Urgente:
-# ! - Tornar tudo da udacity Fixo! Então provavelmente vou dar um split!
+# 1.
 
 # !!!Nova Regra a ser implementada:
 # 1. 
 
 # Afazeres:
+# 0. Criar uma página para as pessoas cadastrarem seu email
 # 1. Quando não tiver o link, criar um link que vai usar a info adquirida para pesquisar no google
 # 2. Criar uma headline com as palavras chaves daquele dia e.g: Python, Java, Hackaton
 # 2. Fazer o git clone quando a imagem for criada
@@ -23,9 +28,11 @@ from datetime import datetime, timedelta
 
 # Expandir o projeto:
 # 1. Descrever os pré requisitos das bolsas de estudo, cursos e competições
-# 3. Enviar por email quando tiver um curso da MJV, Dio ou Semantix Academy
+# 3.1. Semantix Academy - Não possui um link específico com os cursos, sendo necessário avaliar outras formas de fazer isso: Talvez pelo linkedin ?
+# 3.2. MJV LAB - mesma questão da 3.1
+# 3.3. Hackaton Brasil - mesma questão da 3.1, talvez pelo instagram?
 # 4. Verificar se a Udemy ou outros sites grandes estão em promoção
-# 5. Verificar competições, como as do Hackaton Brasil
+# 5. Verificar competições
 
 # Feitos:
 # 1. Base de dados que futuramente poderá ser usada para um modelo de NLP.
@@ -60,8 +67,66 @@ def create_line_to_email(desc,link):
         linha += f'target="_blank" href={link}>Link</a></p></li>'
     return linha
 
-def data():
-    return datetime.today().strftime('%Y-%m-%d')
+def data(fin='mongoDB', yesterday=False):
+    data = datetime.today()
+    if yesterday:
+        data -= timedelta(days=1)
+
+    if fin == 'mongoDB':
+        data_format = data.strftime('%Y-%m-%d')
+    else:
+        data_format = data.strftime('%d/%m/%Y')
+    return data_format
+
+def dic_to_mail_and_db(dic,json_para_collection=json_para_collection,data_hj=data_hj):
+    texto_temp = ''
+    for k,val in dic:
+        # if len(line) < 3:
+        #     continue
+        v = val.strip('\n')
+        texto_temp += create_line_to_email(k,val)
+
+        item_json = {'info':k,
+                'url':val,
+                'data':data_hj}
+
+        json_para_collection.append(item_json)
+
+    texto_temp += '</ul>'
+    return texto_temp
+
+def google_scrapping(soup,dic_exp):
+    
+    links = soup.html.find_all('div',attrs={'class':'egMi0 kCrYT'})
+
+    if links == []:
+        return 
+
+    for x in links:
+        link = x.find('a')
+        desc = x.find('div',attrs={'class':'BNeawe vvjwJb AP7Wnd'})
+
+        link = re.search('href="/url\?q=(.+)&amp;sa=U&amp', str(link)).group(1)
+        desc = re.search(">(.+)</div>", str(desc)).group(1)
+
+        dic_exp[desc] = link
+    
+    return dic_exp
+
+def expansao():
+
+    sites = ['udacity.com%2Fscholarships','dio.me%2Fbootcamp']
+    dic_exp = {}
+    data_hoje = data()
+
+    for site in sites:
+        url = f'https://www.google.com/search?q=allinurl%3A%22https%3A%2F%2F{site}%22++after%3A{data_hoje}'
+        page=requests.get(url)
+        soup=BeautifulSoup(page.text,'html.parser')
+        google_scrapping(soup,dic_exp)
+
+    return dic_exp 
+
 
 def download_resources_links():
     path = '/opt/airflow/dags/free_monthly_learning_resources/resources/readme.md'
@@ -78,54 +143,50 @@ def download_resources_links():
         for i,x in enumerate(links_atualizados):
             if x[:3] == '###':
                 link = links_atualizados[i+1]
+                chave = x.strip('# \n')
                 if link[:5] == 'https':
-                    sites_e_urls[x] = link
+                    sites_e_urls[chave] = link
                 else:
-                    sites_e_urls[x] = 'Sem Link!'
-
+                    sites_e_urls[chave] = 'Sem Link!'
 
     with open('/opt/airflow/dags/links_novos.md', 'r', encoding='utf-8') as links_desatualizados:
         links_ontem = links_desatualizados.readlines()
         # Comparar 'links_hoje' com 'links_ontem':
-        links_a_verificar = [link_hj for link_hj in sites_e_urls.keys() if link_hj not in links_ontem]
-        if links_a_verificar == []:
-            return 'sem_novidades'
+        links_a_verificar_base = {link_hj_k:link_hj_v for link_hj_k,link_hj_v in sites_e_urls.items() if link_hj_k not in links_ontem}
 
     with open('/opt/airflow/dags/links_novos.md', 'w', encoding='utf-8') as links_a_atualizar:
+        # Mantendo o arquivo local atualizado:
         for line in links_atualizados:
             links_a_atualizar.write(line)
 
+    dic_exp = expansao()
+    if links_a_verificar_base == {} == dic_exp:
+        return 'sem_novidades'
+
+
     # Caso um dia decida-se salvar os dados localmente, será usado esse arquivo:
-    with open('/opt/airflow/links_a_verificar.md', 'a', encoding='utf-8') as links_a_verificar_file:
+    # with open('/opt/airflow/links_a_verificar.md', 'a', encoding='utf-8') as links_a_verificar_file:
 
-        collection_urls = mongoCollection()
-        json_para_collection = []
-        data_hj = data()
+    collection_urls = mongoCollection()
+    json_para_collection = []
+    data_hj = data()
 
-        texto_email= '<ul>'
-        for line in links_a_verificar:
-            if len(line) < 3:
-                continue
-            desc = line.strip('# \n')
-            k,v = desc, sites_e_urls[line].strip('\n')
-            texto_email += create_line_to_email(k,v)
+    if dic_exp != {}:
+        texto_email = '<h3>Expansão</h3><ul>'
+        texto_email += dic_to_mail_and_db(dic_exp)
 
-            item_json = {'info':k,
-                    'url':v,
-                    'data':data_hj}
+    if links_a_verificar_base != {}:
+        texto_email = '<h3>Base</h3><ul>'
+        texto_email += dic_to_mail_and_db(links_a_verificar_base)
 
-            json_para_collection.append(item_json)
-
-        texto_email += '</ul>'
-        collection_urls.insert_many(json_para_collection)
-
+    collection_urls.insert_many(json_para_collection)
 
     return texto_email
 
 def send_email_basic(receiver=lista_mails):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    sender_email = 'sender@gmail.com'  # Enter your address
+    sender_email = 'Recursos por Email <uiuiunitimu@gmail.com>'  # Enter your address
     receiver_email = receiver  # Enter receiver address
     password = 'goodpassword' # Enter your gmail password
     email_html = download_resources_links()
@@ -138,7 +199,8 @@ def send_email_basic(receiver=lista_mails):
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
     message.attach(part2)
-    message["Subject"] = f'Novos recursos do dia! ({data()})'
+    data_mail = data(fin='mail')
+    message["Subject"] = f'Novos recursos do dia! ({data_mail})'
     message["From"] = sender_email
 
     context = ssl.create_default_context()
